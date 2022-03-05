afb860bb43b116e943ed36fc09f9a4d7f9ab5649
cedounga <cedounga@gmail.com>
2022-03-05 16:15:13 -0500

    variable.sh

---
+++ variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this script teach how to creaet a variable
+#Author: Cedrick Kalukuta
+
+#Date : 3/5/2022
+
+NAME=Cedrick
+echo -e "\n $NAME is a harworking guy \n"
+
+echo -e "\n The manager says, he will hire $NAME  in his new team wherever he goes\n"
+
+echo -e "\n $NAME likes to show off because the  manager loves him \n"
+
+echo -e "\n $NAME  will always be available when they need him \n"
